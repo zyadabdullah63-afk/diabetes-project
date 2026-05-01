1bfbfb0edc2dcf82ce92dde1e819cbeb6ef6fefa
zyadabdullah63-afk <zyadabdullah63@gmail.com>
2026-05-01 19:22:59 +0300

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,1 +1,37 @@
+function addMessage(msg, sender) {
+    const box = document.getElementById("chatBox");
+    const div = document.createElement("div");
+    div.innerText = sender + ": " + msg;
+    box.appendChild(div);
+}
 
+function sendMessage() {
+    const input = document.getElementById("userInput");
+    const message = input.value;
+
+    addMessage(message, "You");
+
+    fetch('/chat', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: message })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log("Response:", data);
+
+        if (data.status === 'success') {
+            addMessage(data.reply, "Bot");
+        } else {
+            addMessage(data.message, "Bot");
+        }
+    })
+    .catch(err => {
+        console.error(err);
+        addMessage("Error connecting to server", "Bot");
+    });
+
+    input.value = "";
+}
